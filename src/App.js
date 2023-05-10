8cb0ab98edcbae4925b7c6e4653303133906d195
Summer <xiaoxipu@umich.edu>
2023-05-09 19:46:07 -0700

    hoverover feature

--- src/App.js
+++ src/App.js
@@ -49,6 +49,9 @@ function App() {
     });
   }, []);
 
+  const clsValue = isNaN(metrics.CLS) ? 'N/A' : metrics.CLS.toFixed(2);
+  const clsShow = isNaN(metrics.CLS) ? 'N/A' : (100 - ((metrics.CLS.toFixed(2) - 0.1) / 0.15) * 30);
+
     return (
         <ThemeProvider theme={theme}>
           <Box id='mainBox' >
@@ -58,30 +61,37 @@ function App() {
                   <Box sx={{ display: "flex", gap: 5 }}>
                     <Metric
                     name={"FCP"}
-                    value={180 * 50 / metrics.FCP}
-                    // value={0.9 * 100}
+                    value={Math.min(97, 100 - ((Math.round(metrics.FCP) - 2500) / 1500) * 20)}
+                    description={`First Contentful Paint: ${Math.round(metrics.FCP)}ms -
+                                  Marks the time at which the first text or image is painted`}
                     //handleClick={handleClick}
                     //isActive={currentMetric === "Performance"}
                     />
                     <Metric
                     name={"LCP"}
-                    value={180 * 50 / metrics.LCP}
-                    // value={0.85 * 100}
+                    value={Math.min(97, 100 - ((Math.round(metrics.LCP) - 2500) / 1500) * 20)}
+                    description={`Largest Contentful Paint: ${Math.round(metrics.LCP)}ms -
+                                  Marks the time at which the largest text or image is painted`}
                     />
                     <Metric
                     name={"CLS"}
-                    value={metrics.CLS}
-                    // value={0.7 * 100}
+                    value={clsValue}
+                    description={`Cumulative Layout Shift: ${clsShow} -
+                                  Measures the movement of visible elements within the viewport`}
                     />
                     <Metric
                     name={"TBT"}
-                    value={metrics.TBT}
-                    // value={0.45 * 100}
+                    value={Math.min(97, 100 - ((metrics.TBT - 200) / 400) * 30)}
+                    description={`Total Blocking Time: ${Math.round(metrics.TBT)}ms -
+                                  Sum of all time periods between FCP and Time to Interactive,\n
+                                  when task length exceeds 50ms`}
                     />
                     <Metric
                     name={"FID"}
-                    value={metrics.FID}
-                    // value={0.6 * 100}
+                    value={100 - ((metrics.FID - 100) / 200) * 30}
+                    description={`First Input Delay: ${Math.round(metrics.FID)}ms -
+                    Measures the time from when a user first interacts with a page to the time when the browser\n 
+                    is actually able to begin processing event handlers in response to that interaction`}
                     />
                   </Box>
                 </div>
