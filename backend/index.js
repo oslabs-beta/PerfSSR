739307907c956e57a44ed0e72622d6f991d0268e
James Ye <james101994@yahoo.com>
2023-05-11 11:44:50 -0700

    get access to first fiber node

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
-let fiberRoot;
+let devTool;
+let rootNode;
 
 console.log("this means the injected file is running");
 //__REACT_DEVTOOLS_GLOBAL_HOOK__ instantiation of React Dev Tools within our app
 // can be accessed by using window.__REACT_DEVTOOLS_GLOBAL_HOOK__
-console.log("window: ", window.__REACT_DEVTOOLS_GLOBAL_HOOK__);
-fiberRoot = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+devTool = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+console.log("RDT instance", devTool);
 
-if (!fiberRoot) {
+if (!devTool) {
   console.log(
     "Unable to grab instance of fiber root. Are you sure Reactt Dev Tools is installed?"
   );
 }
-console.log(fiberRoot);
+
+
+rootNode = devTool.getFiberRoots(1).values().next().value;
+console.log(rootNode);
