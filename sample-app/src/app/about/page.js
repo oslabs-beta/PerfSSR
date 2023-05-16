0c624f37d6f5b1aa92814dfa3620c086185609e1
Jessica <vo.jessicam@gmail.com>
2023-05-16 12:32:34 -0700

    update repo and about pages to include more fetch calls for testing

--- sample-app/src/app/about/page.js
+++ sample-app/src/app/about/page.js
@@ -4,7 +4,13 @@ export const metadata = {
 
 import React from 'react'
 
-const About = () => {
+const About = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+
+  // await new Promise((resolve) => setTimeout(resolve, 3000)); 
+  const testJson = await response.json();
+  console.log("testjson", testJson);
+
   return (
     <div>
       <h1>About Page</h1>
--- sample-app/src/app/code/repos/page.js
+++ sample-app/src/app/code/repos/page.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import Link from "next/link";
 import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
+// import perfssrFetch from '@/perfssr/fetch-lib';
+
+
 
 async function fetchRepos() {
+  // const response = await fetch('https://api.github.com/users/bradtraversy/repos');
   const response = await fetch('https://api.github.com/users/bradtraversy/repos');
-  
-  await new Promise((resolve) => setTimeout(resolve, 3000)); 
 
+  // await new Promise((resolve) => setTimeout(resolve, 3000)); 
   const repos = await response.json();
   return repos;
 }
 
 const ReposPage = async () => {
     const repos = await fetchRepos();
-    console.log(repos);
+    // console.log(repos);
   return (
     <div className="repos-container">
       <h2>Repositories</h2>
