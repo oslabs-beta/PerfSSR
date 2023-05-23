060027de601d2ffcf382eff88dcd849a3b7525b0
Jessica <vo.jessicam@gmail.com>
2023-05-22 19:31:04 -0700

    change perfssr script entry point

--- sample-app/next.config.js
+++ sample-app/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode:true
+};
 
-module.exports = nextConfig
+module.exports = async phase => {
+    console.log('Starting phase ', phase);
+    return nextConfig;
+};
