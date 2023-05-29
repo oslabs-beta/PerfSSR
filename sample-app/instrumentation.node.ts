d7f928d429234e4c1f0fb426536c1a3df50cb4fc
James Ye <james101994@yahoo.com>
2023-05-28 17:16:39 -0700

    improved documentation of instrumentation files
    Co-authored-by: Jessica jessicavo@github.com

--- sample-app/instrumentation.node.ts
+++ sample-app/instrumentation.node.ts
@@ -5,13 +5,19 @@ import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions"
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
 
 const sdk = new NodeSDK({
+  //entity producing resource attributes
+  //next already as otel built in, we are listening to their built-in instrumentation
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: "next-app",
   }),
+  //bundles all trace data into span objects
   spanProcessor: new SimpleSpanProcessor(
     new OTLPTraceExporter({
-      url: `http://localhost:4000`, //export traces as http req to custom express server on port 400
+      //all traces exported to express server on port 4000
+      url: `http://localhost:4000`,
     })
   ),
 });
+
+//start tracing
 sdk.start();
--- sample-app/instrumentation.ts
+++ sample-app/instrumentation.ts
@@ -1,4 +1,5 @@
 export async function register() {
+  //instrumentation only works on node runtime not edge
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./instrumentation.node");
   }
